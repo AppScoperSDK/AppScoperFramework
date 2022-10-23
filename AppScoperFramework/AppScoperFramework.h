7628be9403d0289356ec78795a0b5f9e1e95fdf9
Mohamed Ibrahim <31712640+MohamedIbrahimHassan@users.noreply.github.com>
2022-10-23 23:25:04 +0200

    Initial Commit

---
+++ AppScoperFramework/AppScoperFramework.h
@@ -0,0 +1,18 @@
+//
+//  AppScoperFramework.h
+//  AppScoperFramework
+//
+//  Created by Mohamed Ibrahim on 23/10/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AppScoperFramework.
+FOUNDATION_EXPORT double AppScoperFrameworkVersionNumber;
+
+//! Project version string for AppScoperFramework.
+FOUNDATION_EXPORT const unsigned char AppScoperFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppScoperFramework/PublicHeader.h>
+
+
